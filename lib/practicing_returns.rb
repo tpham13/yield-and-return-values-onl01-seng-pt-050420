9fa5252d338307c769831810ffb5d5e86ef78a8a
Thanh Pham <tpham0123@gmail.com>
2020-05-14 23:47:53 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def hello(array)
   i = 0
@@ -8,4 +8,4 @@ def hello(array)
   end
 end
 
-\Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }\Tom", "Jim"]) { |name| puts "Hi, #{name}" }
